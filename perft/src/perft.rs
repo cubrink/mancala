b7f73ec7bd441401dde033f6b76d73d50baebe2a
Curtis Brinker <curtisbrnkr@gmail.com>
2025-08-26 00:30:52 -0500

    fix: Include completed nodes in `perft` count

--- perft/src/perft.rs
+++ perft/src/perft.rs
@@ -29,7 +29,7 @@ pub fn perft_divide(game: &Mancala, depth: usize) -> [usize; 6] {
 }
 
 pub fn perft(game: &Mancala, depth: usize) -> usize {
-    if depth == 0 {
+    if depth == 0 || game.is_completed() {
         // If no depth to search, then we are just at this node
         return 1;
     } else if depth == 1 {
