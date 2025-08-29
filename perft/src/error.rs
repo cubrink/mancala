39fb9d9963678a41a526d42680360149fa89cbd8
Curtis Brinker <curtisbrnkr@gmail.com>
2025-08-28 21:58:19 -0500

    Error on invalid start sequence

--- perft/src/error.rs
+++ perft/src/error.rs
@@ -5,4 +5,7 @@ pub enum PerftError {
     /// An action provided is invalid
     #[error("The action '{0}' is not a valid action.")]
     InvalidAction(String),
+    /// The starting move sequence is not valid.
+    #[error("Invalid starting sequence '{0:?}'")]
+    InvalidStart(Vec<usize>),
 }
