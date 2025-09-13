8d22bd33306afa3c4b2cc0164ab80d06ff3f79c5
Curtis Brinker <curtisbrnkr@gmail.com>
2025-09-13 16:29:46 -0500

    build: Add instruction and wall benchmarks

---
+++ perft/benches/walltime.rs
@@ -0,0 +1,36 @@
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+use mancala_lib::Mancala;
+use perft::perft;
+use std::hint::black_box;
+
+fn perft_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("perft");
+
+    for depth in [0, 1, 2, 3, 5] {
+        group.bench_with_input(BenchmarkId::new("depth", depth), &depth, |b, &depth| {
+            b.iter_with_setup(
+                || Mancala::default(),
+                |game| black_box(perft(black_box(&game), black_box(depth))),
+            )
+        });
+    }
+
+    group.finish();
+}
+
+fn perft_benchmark_no_blackbox(c: &mut Criterion) {
+    let mut group = c.benchmark_group("perft_no_blackbox");
+
+    for depth in [0, 1, 2, 3, 5, 8] {
+        group.bench_with_input(BenchmarkId::new("depth", depth), &depth, |b, &depth| {
+            b.iter_with_setup(
+                || Mancala::default(), // Setup - not measured
+                |game| perft(black_box(&game), black_box(depth)),
+            )
+        });
+    }
+
+    group.finish();
+}
+criterion_group!(benches, perft_benchmark, perft_benchmark_no_blackbox);
+criterion_main!(benches);
